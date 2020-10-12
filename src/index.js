6702201fc008136155a4c787f8dbd6099d4f2e4a
adam-larosa <adam.larosa@flatironschool.com>
2020-10-12 12:48:13 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const toyFormContainer = document.querySelector(".container");
 const addBtn = document.querySelector("#new-toy-btn");
 const toyForm = document.querySelector('form.add-toy-form')
 
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+
 document.addEventListener("DOMContentLoaded", () => {
 	addBtn.addEventListener("click", () => {
 		// hide & seek with the form
@@ -19,12 +23,12 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 	toyForm.addEventListener('submit', e => {
 		e.preventDefault()
-		handleForm(e)
+		submitForm(e)
 	})
 	getToys();
 });
 
-const handleForm = (e) => {
+const submitForm = (e) => {
 	addToy = !addToy
 	addBtn.innerText = "Add a new toy!"
 	e.target.parentElement.style = 'none'
@@ -60,34 +64,88 @@ const renderToyCard = (toy) => {
 	div.querySelector('h2').innerText = toy.name
 	div.querySelector('img').src = toy.image
 	div.querySelector('img').className = "toy-avatar"
-	div.querySelector('p').innerText = `${toy.likes} ${toy.likes > 1 ? "likes" : "like"}`
+	div.querySelector('p').innerText = `${likeContent(toy.likes)}`
 	div.querySelector('button').className = 'like-btn'
 	div.querySelector('button').innerText = 'Like <3'
 	div.querySelector('button').addEventListener('click', () => increaseLikes(toy.id))
+//begin addition of container for edit & delete buttons
+	buttonContainer = document.createElement('div')
+//end of addition
 //begin addition of delete button
 	deleteButton = document.createElement('button')
 	deleteButton.className = 'delete-btn'
 	deleteButton.innerText = 'destroy'
-	deleteButton.addEventListener('click', e => {
-		deleteToy(e)
-	})
-	div.appendChild(deleteButton)
+	deleteButton.addEventListener('click', e => deleteToy(e))
+	buttonContainer.appendChild(deleteButton)
 //end of addition
+//begin addition of edit button
+	editButton = document.createElement('button')
+	editButton.className = 'edit-btn'
+	editButton.innerText = 'edit'
+	editButton.addEventListener('click', e => editToy(e))
+	buttonContainer.appendChild(editButton)
+//end of addition
+	div.appendChild(buttonContainer)
 	toyContainer.appendChild(div)
 
 }
 
-const deleteToy = (e) => {
-	fetch(`${toyTarget}/${e.target.parentElement.id}`, {
-		method: 'DELETE',
-	}).then(r => r.json()).then(j => {
-		document.getElementById(e.target.parentElement.id).remove()
+const editToy = e => {
+	id = e.target.parentElement.parentElement.id
+	modal.style.display = "block";
+	span.onclick = () => modal.style.display = "none"
+	window.onclick = e => {if (e.target == modal) modal.style.display = "none"}
+	form = document.getElementById('editForm')
+	form.addEventListener('submit', e => {
+		e.preventDefault()
+		fetch(`${toyTarget}/${id}`, {
+			method: 'PATCH',
+			headers: {
+				"Content-Type": "application/json",
+				  Accept: "application/json"
+			},
+			body: JSON.stringify({
+				name: e.target.name.value,
+				image: e.target.image.value
+			})
+		}).then(r => r.json()).then(j => {
+			card = document.getElementById(id)
+			card.querySelector('h2').innerText = j.name
+			card.querySelector('img').src = j.image
+			likes = card.querySelector('p').innerText.split(' ')
+			likes[0] = j.likes
+			card.querySelector('p').innerText = likes.join(' ')
+		})
+		modal.style.display = "none";
 	})
+}
+
+const deleteToy = (e) => {
+	fetch(`${toyTarget}/${e.target.parentElement.parentElement.id}`, {
+		method: 'DELETE',
+	}).then(() => {
+		document.getElementById(e.target.parentElement.parentElement.id).remove()
+	})
+}
+
+const likeContent = (likes) => {
+	if (likes == 0) {
+		return 'No one likes this toy.'
+	} else if (likes > 1) {
+		return `${likes} likes`
+	} else {
+		return `${likes} like`
+	}
 }
 
 const increaseLikes = id => {
 	div = document.getElementById(id)
 	likes = div.querySelector('p').innerText.split(' ')
+	if (likes[0] == "No") {
+		likes = []
+		likes[0] = 0
+		likes[1] = "like"
+	}
 	if (likes[0] == 'null') likes[0] = 0
 	fetch(`${toyTarget}/${id}`, {
 		method: "PATCH",
@@ -97,11 +155,9 @@ const increaseLikes = id => {
 		body: JSON.stringify({
 			likes: parseInt(likes[0]) + 1
 		})
-	}).then(resp => resp.json()).then(json  => {
-		likes[0] = json.likes
-		likes[0] > 1 ? likes[1] = 'likes' : likes[1] = 'like'
-		div.querySelector('p').innerText = likes.join(' ')
-	})//end of fetch
+	}).then(resp => resp.json()).then(j  => {
+		div.querySelector('p').innerText = likeContent(j.likes == 'null' ? 0 : j.likes)
+	})
 }
 
 const getToys = () => {
